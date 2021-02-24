3cdbe4ceed87275b8fcc17eb0f6f3bd4e6b34725
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-02-23 23:02:26 -0600

    updated username and password for jawsdb connection

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=ToDoList';
-    $username='root';
+    $dsn = 'mysql:host=phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ze3n7uyhcoywmp5a';
+    $username='eygt4158xbq3bip0';
+    $password='tjl94cvi1so3n1hv';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         include('view/error.php');
     }
