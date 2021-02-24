53f775f25823eea6bdffa2438e519843fe70390b
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-02-24 01:52:30 -0600

    Changed database credentials back to localhost

--- model/database.php
+++ model/database.php
@@ -1,17 +1,10 @@
 <?php
-
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
-
-    $dsn = "mysql:host=$hostname;dbname=$database";
+    
+    $dsn = 'mysql:host=localhost;dbname=ToDoItems';
+    $username = 'root';
 
     try {
-        $db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username);
     } catch (PDOException $e) {
         include('view/error.php');
     }
