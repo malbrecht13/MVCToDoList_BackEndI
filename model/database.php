0005c6b307b3882039722e563bf4c575c0c77cd3
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-02-23 23:34:37 -0600

    Added database info to config.php

--- model/database.php
+++ model/database.php
@@ -1,8 +1,6 @@
 <?php
 
-    $dsn = 'mysql:host=phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ze3n7uyhcoywmp5a';
-    $username='eygt4158xbq3bip0';
-    $password='tjl94cvi1so3n1hv';
+    require('config.php');
 
     try {
         $db = new PDO($dsn, $username, $password);
